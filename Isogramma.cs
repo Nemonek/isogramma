237fa2a77914c409db00e9e0f8330ea409f754e8
Riccardo Piredda <127589992+Nemonek@users.noreply.github.com>
2023-05-09 18:23:45 +0200

    Ottimizzazione

--- Isogramma.cs
+++ Isogramma.cs
@@ -7,37 +7,34 @@ public static class Isogramma
     public static bool Verifica(string word)
     {
         word = word.ToLower();
-        int i = 1;
-        char tmp=' '; //variabile di appoggio
-        int lenght1 = 0;
+        int LettereRimosse = 1; //Contatore di quante lettere vengono teoricamente rimosse (se non ci sono ripetizioni si rimuove una lettera alla volta)
+        int Length = 0;         //Lunghezza della stringa senza simboli e spazi
 
-        //creazione di una funzione che ricostruisce la parola rimuovendo i simboli e usando lo strongbuilder
-        void rebuilder(ref string word, ref char tmp) {
-            StringBuilder sb = new StringBuilder();
-            foreach (string item in word.Split('.', '-', ' ', '"' ,tmp))
-                sb.Append(item);
-            word = sb.ToString();
-        }
+
         
-        // parametri passati per referenza
-        rebuilder(ref word, ref tmp);
-        lenght1 = word.Length;
+        rebuilder(ref word);    //Prima chiamata alla funzione rebuilder per pulire la stringa da spazi e simboli se presenti
+        Length = word.Length;
+        
         // Il foreach scorre la parola per ogni char presente, poi passa il char alla funzione rebuilder che
         // splitta nuovamente la parola ma questa volta con un parametro diverso: il char assegnato a tmp.
         // se la lunghezza dopo l'esecuzione di rebuilder è diversa dalla lunghezza - i (i viene incrementata a ogni
         // ciclo per tenere traccia dei char rimossi man mano) ritorna falso
         foreach (char item in word)
         {
-            tmp = item;
-            rebuilder(ref word, ref tmp);
-            if (word.Length != lenght1-i)
-            {
+            rebuilder(ref word, item);
+            if (word.Length != Length-LettereRimosse)
                 return false;
-            }
-            i++;
+            LettereRimosse++;
         }
 
         return true;
     }
 
+        //Ricostruisce la stringa dopo lo split
+        private static void rebuilder(ref string word, char c = ' ') {
+            StringBuilder sb = new StringBuilder();
+            foreach (string item in word.Split('.', '-', '"' ,c))
+                sb.Append(item);
+            word = sb.ToString();
+        }
 }
